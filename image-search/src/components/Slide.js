abc8dacb35d8d1ec0fb7bfa1bdc0d1b19a4e9948
ApoorvaJu08 <apoorva10339@gmail.com>
2020-07-10 21:10:54 +0530

    making responsive left

--- image-search/src/components/Slide.js
+++ image-search/src/components/Slide.js
@@ -9,40 +9,29 @@ const Loading = () => (
 )
 
 export default function Slideshow({images, interval=3000}){
-    let [prevImage, setPrevImage] = useState(0);
     let [currImage, setCurrImage] = useState(0);
-    let [nextImage, setNextImage] = useState(1);
 
     function prev() {
-        if(prevImage === 0){
-            // nextImage = images[thumbnails.length - 1].urls.regular;
+        if(currImage === 0){
+            setCurrImage(currImage);
         }
         else{
-            setPrevImage(prevImage - 1);
             setCurrImage(currImage - 1);
-            setNextImage(nextImage - 1);
         }
     }
 
     function next() {
-        if(nextImage === images.length - 1){
-            // nextImage = images[thumbnails.length - 1].urls.regular;
+        if(currImage === images.length - 1){
+            setCurrImage(currImage);
         }
         else{
-            setPrevImage(prevImage + 1);
             setCurrImage(currImage + 1);
-            setNextImage(nextImage + 1);
         }
     }
 
     return (
         <section className="slideshow animate__animated animate__fadeInRight">
             <div className="slide-holder">
-                {/* <section className="slide previous-slide">
-                    <div className="slide-thumbnail">
-                        <img src={images[prevImage].urls.regular} className="unsplash-image"></img>
-                    </div>
-                </section> */}
                 <LazyLoad key={currImage} placeholder={<Loading />}>
                     <section className="slide current-slide">
                         <div className="slide-thumbnail">
@@ -50,16 +39,9 @@ export default function Slideshow({images, interval=3000}){
                         </div>
                     </section>
                 </LazyLoad>
-                
-                {/* <section className="slide current-slide">
-                    <div className="slide-thumbnail">
-                        <img src={images[nextImage].urls.regular} className="unsplash-image"></img>
-                    </div>
-                </section>     */}
             </div>          
             <div className="slideshow-controller">
                 <span onClick = {prev}>Previous</span>
-                {/* <span onClick={changeSlide("next")}>Next</span> */}
                 <span onClick = {next}>Next</span>
             </div>
         </section>
