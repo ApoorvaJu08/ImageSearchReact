63f8e8d312f830db876dd9af4e83608acfec5fd7
ApoorvaJu08 <apoorva10339@gmail.com>
2020-07-13 13:15:53 +0530

    form styling changed

--- image-search/src/components/Signin.js
+++ image-search/src/components/Signin.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 const SignIn = () => {
     return(
-        <h4>SignIn</h4>
+        <div className="mycard">
+          <div className="card auth-card input-field">
+            <h2 className="form-heading">Login</h2>
+            <input
+            type="text"
+            placeholder="email"
+            />
+            <input
+            type="password"
+            placeholder="password"
+            />
+            <button className="btn waves-effect waves-light btn-beneath-input">
+                Login
+            </button>
+        </div>
+      </div>
     )
 }
 
